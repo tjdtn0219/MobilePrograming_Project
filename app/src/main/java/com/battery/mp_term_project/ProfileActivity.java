14f05a91bf0d549f32558fdcac21ac59b500759e
kjh0710 <kjh710319@naver.com>
2022-04-04 14:35:18 +0900

    프로필 게시물 작성 추가

---
+++ app/src/main/java/com/battery/mp_term_project/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.battery.mp_term_project;
+
+import android.os.Bundle;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    SearchView contentSearchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        
+    }
+}
