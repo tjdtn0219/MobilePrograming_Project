7920950d31500c1e43a813dcb5f14a08d36047e5
장성수 <tjdtn0219@ajou.ac.kr>
2022-04-05 21:27:01 +0900

    main xml & main activity 수정

--- app/src/main/java/com/battery/mp_term_project/MainActivity.java
+++ app/src/main/java/com/battery/mp_term_project/MainActivity.java
@@ -2,17 +2,31 @@ package com.battery.mp_term_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, ChattingActivity.class);
-        startActivity(intent);
+        addContent();
+        addContent();
+        addContent();
     }
+
+    private void addContent() {
+        container = findViewById(R.id.contentscontainer);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.content, container, true);
+    }
+
 }
