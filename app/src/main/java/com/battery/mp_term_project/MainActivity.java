2925b0fd6f8ce0c1a7a2b094acd5762c530bc597
Jerry <tom59651@gmail.com>
2022-04-08 21:17:45 +0900

    메인 Activity에서 다른 Activity로 넘어가는 기능들 추가

--- app/src/main/java/com/battery/mp_term_project/MainActivity.java
+++ app/src/main/java/com/battery/mp_term_project/MainActivity.java
@@ -3,9 +3,11 @@ package com.battery.mp_term_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +22,36 @@ public class MainActivity extends AppCompatActivity {
         addContent();
         addContent();
         addContent();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_actionbar_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int itemId = item.getItemId();
+        if (itemId == R.id.item_search)
+        {
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
+        }
+        else if (itemId == R.id.item_upload)
+        {
+            Intent intent = new Intent(this, UploadActivity.class);
+            startActivity(intent);
+        }
+        else if (itemId == R.id.item_profile)
+        {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            startActivity(intent);
+        }
+
+        return false;
     }
 
     private void addContent() {
